cc8af32b2dfbd269b93459d6343e6f942aea4442
Jefferson Escobar <jeffersoncamilo.esc@gmail.com>
2025-01-15 19:05:06 -0500

    Ejercicio 2

---
+++ ej2.js
@@ -0,0 +1,13 @@
+var nombre;
+var apellido;
+var edad;
+var fechaNacimiento;
+var direccion;
+
+nombre = 'Jefferson';
+apellido = 'Escobar';
+edad = 25;
+fechaNacimiento = '2000-04-23';
+direccion = 'Calle 123 # 45-67';
+
+
