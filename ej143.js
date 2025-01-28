df00785b3d8d5a1bea82e3b5ee0a779308e0d757
Jefferson Escobar <jeffersoncamilo.esc@gmail.com>
2025-01-27 19:48:39 -0500

    Ej Objetos 144

--- ej143.js
+++ ej143.js
@@ -1,1 +1,3 @@
-let usuario = {};
+const usuario = {};
+
+console.log(usuario);
---
+++ ej144.js
@@ -0,0 +1,7 @@
+const usuario = {
+    username: "batman",
+    password: "Alfred1960KPO!"
+};
+
+console.log(usuario.username);
+console.log(usuario.password);
