6499b9c3c7814161244c40f7c4250f5aee2edc38
Jefferson Escobar <jeffersoncamilo.esc@gmail.com>
2025-04-14 18:20:51 -0500

    odificaciones

--- js/ej35.js
+++ js/ej35.js
@@ -3,4 +3,13 @@ let numeroDeClienteIngresado = '143245';
 
 // Comparación por valor
 
-let esCorrecto = ( numeroDeClie
+let esCorrecto = ( numeroDeCliente == numeroDeClienteIngresado);
+
+// Comparación por valor y tipo
+
+let esValido = ( numeroDeCliente === numeroDeClienteIngresado);
+
+// Mostrar resultados en consola
+
+console.log("El número ingresado por el cliente es correcto:", esCorrecto);
+console.log("El número ingresado por el cliente es válido:", esValido);
